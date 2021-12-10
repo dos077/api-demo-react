458640adfad9f049dc5eec20df589adaca0f7bed
Zh <zeming@dreamofsky.com>
2021-12-10 11:43:06 -0500

    added fields to acc page

--- src/components/AccPanel.js
+++ src/components/AccPanel.js
@@ -2,9 +2,24 @@ import React, { useState } from 'react';
 
 const AccPanel = (props) => {
   const [screenStr, setScreenStr] = useState('');
+  const [fields, setFields] = useState([]);
+  
+  const FieldsOptions = () =>
+    ['positions', 'orders']
+      .map(option => <option key={option} value={option}>{option}</option>);
+
+  const handleOptions = (e) => {
+    const newSelection = [];
+    const options = e.target;
+    for (let i = 0; i < options.length; i++) {
+      const { selected, value } = options[i];
+      if (selected) newSelection.push(value);
+    }
+    setFields(newSelection);
+  }
   
   const getAccs = async () => {
-    const resJson = await props.endPoint.getAccounts();
+    const resJson = await props.endPoint.getAccounts({ fields });
     console.log(resJson);
     setScreenStr(JSON.stringify(resJson));
   }
@@ -13,6 +28,15 @@ const AccPanel = (props) => {
     <section>
       <h2>Account Info</h2>
       <p>
+        <label>fields</label>
+        <select
+          value={fields} 
+          onChange={(e)=> {handleOptions(e)}}
+          multiple={true}
+          size="2"
+        >
+          <FieldsOptions />
+        </select>
         <button onClick={getAccs}>get accounts</button>
       </p>
       <p>{screenStr}</p>
--- src/components/ConfigPanel.js
+++ src/components/ConfigPanel.js
@@ -5,10 +5,25 @@ const ConfigPanel = (props) => {
   const [redirectUri, setRedirectUri] = useState('https://dos077.github.io/api-demo-react/');
   const [storeAuth, setStoreAuth] = useState(true);
   const [logInRedirect, setLogInRedirect] = useState(true);
+  const [scope, setScope] = useState([]);
+  
+  const ScopeOptions = () =>
+    ['PlaceTrades', 'AccountAccess', 'MoveMoney']
+      .map(option => <option key={option} value={option}>{option}</option>);
+
+  const handleOptions = (e) => {
+    const newSelection = [];
+    const options = e.target;
+    for (let i = 0; i < options.length; i++) {
+      const { selected, value } = options[i];
+      if (selected) newSelection.push(value);
+    }
+    setScope(newSelection);
+  }
 
   const buildClient = () => {
     props.buildClient({
-      clientId, redirectUri, storeAuth, logInRedirect
+      clientId, redirectUri, storeAuth, logInRedirect, scope
     });
   }
 
@@ -23,6 +38,15 @@ const ConfigPanel = (props) => {
       <input type="checkbox" checked={storeAuth} onChange={(e) => setStoreAuth(e.target.checked)} />
       <label>Redirect for Login</label>
       <input type="checkbox" checked={logInRedirect} onChange={(e) => setLogInRedirect(e.target.checked)} />
+      <label>scope</label>
+      <select
+        value={scope} 
+        onChange={(e)=> {handleOptions(e)}}
+        multiple={true}
+        size="3"
+      >
+        <ScopeOptions />
+      </select>
       <button onClick={buildClient} value="initialize client">
         initialize client
       </button>
